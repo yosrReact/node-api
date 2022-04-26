cf6d8b0d3b3147d0e9e45df0fbe38aa90b25ef44
Yosr Naija <yosr.naija@live.com>
2022-04-26 08:58:12 +0100

    fix url

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   // const url='mongodb+srv://yosr:yosr@cluster0.6kres.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   //  const url =
   //    "mongodb+srv://yosr:yosr@cluster0.hfl1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  const url = "mongodb://localhost:27017/test"
+  const url = "mongodb://http://softprecision.website:27017/test"
   const conn = await mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
