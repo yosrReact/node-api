544851967864343cd0e77406cc437c3b4ed31c1a
Yosr Naija <yosr.naija@live.com>
2022-04-27 01:37:34 +0100

    fix db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   //  const url =
   //    "mongodb+srv://yosr:yosr@cluster0.hfl1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   // const url = "mongodb://softprecision.website:27017/test"
-  const url = "mongodb://citizix:S3cret@mongodb:27017/test"
+  const url = "mongodb://mongodb:27017/test"
   const conn = await mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
